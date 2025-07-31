0e19b6d0211af848edc077cf8e635c1163960be5
Justin Magaram <justin@magaram.com>
2025-07-30 23:27:16 -0700

    reorder navigation links in Header component

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,8 +5,8 @@ const Header = () => {
     <header>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/orgs">Organizations</Link>
         <Link to="/donations">Donations</Link>
+        <Link to="/orgs">Organizations</Link>
       </nav>
     </header>
   );

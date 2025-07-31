a2aad44accf16caec798bee9f3b32e3d16e925f9
Justin Magaram <justin@magaram.com>
2025-07-30 17:33:48 -0700

    Organizations header button

--- src/Header.tsx
+++ src/Header.tsx
@@ -6,6 +6,7 @@ const Header = () => {
       <nav>
         <Link to="/">Home</Link>
         <Link to="/orgs">Organizations</Link>
+        <Link to="/donations">Donations</Link>
       </nav>
     </header>
   );

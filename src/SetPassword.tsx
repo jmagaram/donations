19e40ac50f2f88f4af3587a04acc983fd24ec91e
Justin Magaram <justin@magaram.com>
2025-08-03 21:22:27 -0700

    improve set password experience

--- src/SetPassword.tsx
+++ src/SetPassword.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import StatusBox from "./StatusBox";
 
 const SetPassword = () => {
   const [password, setPassword] = useState("");
@@ -34,31 +35,30 @@ const SetPassword = () => {
     <div>
       <h1>Set password</h1>
       {hasPassword && (
-        <p>
+        <p className="instructions">
           A password is required to use this web site. You will know if it is
-          set correctly when you attempt to sync or change some data. If the
+          set correctly when you try to sync or change some data. If the
           password is incorrect, a security error message will be displayed and
           you will then have an opportunity to try again.
         </p>
       )}
       {!hasPassword && (
-        <p>
+        <p className="instructions">
           A password is required to use this web site; please type it below. You
-          will know if it is set correctly when you attempt to sync or change
-          some data. If the password is incorrect, a security error message will
-          be displayed and you will then have an opportunity to try again.
+          will know if it is set correctly when you try to sync or change some
+          data. If the password is incorrect, a security error message will be
+          displayed and you will then have an opportunity to try again.
         </p>
       )}
       <form onSubmit={handleSubmit} style={{ marginBottom: "1rem" }}>
         <div className="form-field">
-          <label htmlFor="password">Password:</label>
+          <label htmlFor="password">Password</label>
           <div className="form-row">
             <input
               type={showPassword ? "text" : "password"}
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              placeholder="Enter password"
             />
             <label
               style={{ display: "flex", alignItems: "center", gap: "0.25rem" }}
@@ -73,7 +73,9 @@ const SetPassword = () => {
           </div>
         </div>
         <div className="toolbar">
-          <button type="submit">Ok</button>
+          <button type="submit" disabled={!password.trim()}>
+            {hasPassword ? "Update password" : "Set password"}
+          </button>
           {hasPassword && (
             <button type="button" onClick={handleClear}>
               Sign out

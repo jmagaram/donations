e1b47f9bd21636af48157f0eceacc9988fdc6e4b
Justin Magaram <justin@magaram.com>
2025-08-05 10:26:53 -0700

    better CSS for filter controls, hide label on small screens

--- src/OrgsView.tsx
+++ src/OrgsView.tsx
@@ -30,27 +30,35 @@ const OrgsView = ({
         <Link to="/orgs/add">Add organization</Link>
       </div>
       <div className="toolbar">
-        <input
-          type="search"
-          id="filter"
-          value={currentTextFilter}
-          onChange={(e) => textFilterChanged(e.target.value)}
-          placeholder="Search"
-        />
-        <div className="toolbar-item medium-screen">
+        <div className="toolbar-item">
+          <label htmlFor="filter" className="toolbar-item">
+            Find
+          </label>
+          <input
+            type="search"
+            id="filter"
+            value={currentTextFilter}
+            onChange={(e) => textFilterChanged(e.target.value)}
+            placeholder="Search"
+          />
+        </div>
+        <div className="toolbar-item medium-screen large-screen">
           {availableCategories.length > 0 && (
-            <select
-              id="categoryFilter"
-              value={currentCategoryFilter}
-              onChange={(e) => categoryFilterChanged(e.target.value)}
-            >
-              <option value="all">All categories</option>
-              {availableCategories.map((category) => (
-                <option key={category} value={category}>
-                  {category}
-                </option>
-              ))}
-            </select>
+            <>
+              <label htmlFor="categoryFilter">Category</label>
+              <select
+                id="categoryFilter"
+                value={currentCategoryFilter}
+                onChange={(e) => categoryFilterChanged(e.target.value)}
+              >
+                <option value="all">All categories</option>
+                {availableCategories.map((category) => (
+                  <option key={category} value={category}>
+                    {category}
+                  </option>
+                ))}
+              </select>
+            </>
           )}
         </div>
         {hasActiveFilters && (

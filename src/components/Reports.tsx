fbf41a1229e8dcee66c5639019787776af18d623
Justin Magaram <justin@magaram.com>
2025-08-15 13:26:54 -0700

    change default report links to "paid" only

--- src/components/Reports.tsx
+++ src/components/Reports.tsx
@@ -7,12 +7,12 @@ const Reports = () => {
       <nav>
         <ul className="nav-list">
           <li>
-            <Link to="/reports/yearlytotals?year=last5">
+            <Link to="/reports/yearlytotals?year=last5&type=paid">
               Organization totals
             </Link>
           </li>
           <li>
-            <Link to="/reports/yearlytotalsbycategory?year=last5">
+            <Link to="/reports/yearlytotalsbycategory?year=last5&type=paid">
               Category totals
             </Link>
           </li>
